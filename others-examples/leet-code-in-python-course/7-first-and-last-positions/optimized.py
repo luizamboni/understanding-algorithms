d5530049a57a7d4f1f58a0653f20d9ba6e8f36bf
Luiz Zamboni <luizamboni2002@hotmail.com>
2024-03-28 15:06:56 -0300

    putting some prints to better understand first-and-last-positions

--- others-examples/leet-code-in-python-course/7-first-and-last-positions/optimized.py
+++ others-examples/leet-code-in-python-course/7-first-and-last-positions/optimized.py
@@ -1,11 +1,12 @@
 nums = [10,11,11,11,14,15]
 
 
-def find_the_first(nums, target):
+def find_first(nums, target):
     left_index = 0
     right_index = len(nums) - 1
     while left_index <= right_index:
         mid = (left_index + right_index) // 2
+        print("find_the_first mid", mid, nums[mid])
         if nums[mid] == target:
             # if is the very first item or is not first occurrence
             if mid == 0 or nums[mid - 1] != target:
@@ -17,11 +18,12 @@ def find_the_first(nums, target):
             left_index = mid + 1
     return -1
 
-def find_the_last(nums, target):
+def find_last(nums, target):
     left_index = 0
     right_index = len(nums) - 1
     while left_index <= right_index:
         mid = (left_index + right_index) // 2
+        # print("find_the_last mid", mid)
         if nums[mid] == target:
             # if is the very last item or is not last in contigous
             if mid == len(nums) - 1 or nums[mid + 1] != target:
@@ -34,9 +36,12 @@ def find_the_last(nums, target):
     return -1
 
 
+def find_first_and_last(nums, target):
+    first = find_first(nums, target)
+    last = find_last(nums, target)
+    return [first, last]
+
 
 print(
-    find_the_first(nums, 11),
-
-    find_the_last(nums, 11)
+    find_first_and_last(nums, 11),
 )
