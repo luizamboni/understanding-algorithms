7c02ed69c4388aa5145fcd93acfa593a0642e742
Luiz Zamboni <luizamboni2002@hotmail.com>
2024-08-29 02:21:40 -0300

    adding EPAM challenge code 1, improved

--- inverviews/epam/case-1.py
+++ inverviews/epam/case-1.py
@@ -1,29 +1,20 @@
 
 def calc_moves(nums):
     incr = 0
-    l = 1
-    l2 = 1
     moves = 0
-    r = len(nums) - 1
 
-    while l2 <= r:
-        print("-> comparing", nums[l2-1] , nums[l2] )
-        if nums[l2] + incr <= nums[l2-1]:
-            incr = nums[l2-1] - nums[l2] + 1
+    for i in range(1, len(nums)):
+        curr=nums[i]
+        prev=nums[i-1]
+        if prev + incr >= curr + incr:
+            incr = (prev + incr) - (curr) + 1
             moves+=1
-        else:
-            # apply the increment to prev elements
-            for j in range(l, l2+1):
-                nums[j]=nums[j]+incr
-            l=l2+1
-        l2+=1
+        # print(incr)
 
-
-    print(nums)
     return moves
 
 input1=[4,2,4,1,3,5]
-input2=[3,5,7,7]
+input2=[3,5, 7,7]
 input3=[1,5,6,10] 
 
 print(
